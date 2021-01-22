0d455c45f00bb42e84e55a687b2c3a44c7cdf45b
María Patricia Serrano Ramírez <201800378@colsanjose.edu.gt>
2021-01-22 07:50:36 -0600

    Creación del Proyecto

---
+++ Calculadora/Calculadora/Operaciones.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    class Operaciones
+    {
+        public Double Suma(double a, double b)
+        {
+            return (a + b);
+        }
+
+        public Double Resta(double a, double b)
+        {
+            return (a - b);
+        }
+
+        public Double Mult(double a, double b)
+        {
+            return (a * b);
+        }
+
+        public Double Div(double a, double b)
+        {
+            return (a / b);
+        }
+
+
+    }
+}
