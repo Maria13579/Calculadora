e4a4aa83a2d1f7361da19bb09a3e2156a3a73222
María Patricia Serrano Ramírez <201800378@colsanjose.edu.gt>
2021-01-25 11:55:03 -0600

    Creación del Proyecto

--- Calculadora/Calculadora/Form1.Designer.cs
+++ Calculadora/Calculadora/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Calculadora
 {
-    partial class Form1
+    partial class frmCalculadora
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,13 +28,169 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblv1 = new System.Windows.Forms.Label();
+            this.lblv2 = new System.Windows.Forms.Label();
+            this.lblopera = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.cmdSuma = new System.Windows.Forms.Button();
+            this.cmdResta = new System.Windows.Forms.Button();
+            this.cmdMulti = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.cmdSalir = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblv1
+            // 
+            this.lblv1.AutoSize = true;
+            this.lblv1.Font = new System.Drawing.Font("Elephant", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblv1.Location = new System.Drawing.Point(199, 60);
+            this.lblv1.Name = "lblv1";
+            this.lblv1.Size = new System.Drawing.Size(114, 21);
+            this.lblv1.TabIndex = 0;
+            this.lblv1.Text = "Primer Valor";
+            // 
+            // lblv2
+            // 
+            this.lblv2.AutoSize = true;
+            this.lblv2.Font = new System.Drawing.Font("Elephant", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblv2.Location = new System.Drawing.Point(199, 117);
+            this.lblv2.Name = "lblv2";
+            this.lblv2.Size = new System.Drawing.Size(124, 21);
+            this.lblv2.TabIndex = 1;
+            this.lblv2.Text = "Segundo Valor";
+            this.lblv2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblopera
+            // 
+            this.lblopera.AutoSize = true;
+            this.lblopera.Font = new System.Drawing.Font("Elephant", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblopera.Location = new System.Drawing.Point(27, 257);
+            this.lblopera.Name = "lblopera";
+            this.lblopera.Size = new System.Drawing.Size(122, 27);
+            this.lblopera.TabIndex = 2;
+            this.lblopera.Text = "Operación";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(334, 61);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(89, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(334, 117);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(89, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // cmdSuma
+            // 
+            this.cmdSuma.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.cmdSuma.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdSuma.Location = new System.Drawing.Point(182, 186);
+            this.cmdSuma.Name = "cmdSuma";
+            this.cmdSuma.Size = new System.Drawing.Size(160, 37);
+            this.cmdSuma.TabIndex = 6;
+            this.cmdSuma.Text = "Suma";
+            this.cmdSuma.UseVisualStyleBackColor = false;
+            // 
+            // cmdResta
+            // 
+            this.cmdResta.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.cmdResta.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdResta.ForeColor = System.Drawing.Color.Black;
+            this.cmdResta.Location = new System.Drawing.Point(182, 229);
+            this.cmdResta.Name = "cmdResta";
+            this.cmdResta.Size = new System.Drawing.Size(160, 37);
+            this.cmdResta.TabIndex = 7;
+            this.cmdResta.Text = "Resta";
+            this.cmdResta.UseVisualStyleBackColor = false;
+            // 
+            // cmdMulti
+            // 
+            this.cmdMulti.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.cmdMulti.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdMulti.Location = new System.Drawing.Point(182, 272);
+            this.cmdMulti.Name = "cmdMulti";
+            this.cmdMulti.Size = new System.Drawing.Size(160, 37);
+            this.cmdMulti.TabIndex = 8;
+            this.cmdMulti.Text = "Multiplicación";
+            this.cmdMulti.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button4.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(182, 315);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(160, 37);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "División";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // cmdSalir
+            // 
+            this.cmdSalir.BackColor = System.Drawing.Color.DarkOrchid;
+            this.cmdSalir.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdSalir.Location = new System.Drawing.Point(508, 379);
+            this.cmdSalir.Name = "cmdSalir";
+            this.cmdSalir.Size = new System.Drawing.Size(120, 67);
+            this.cmdSalir.TabIndex = 10;
+            this.cmdSalir.Text = "Salir";
+            this.cmdSalir.UseVisualStyleBackColor = false;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.BackColor = System.Drawing.Color.BlueViolet;
+            this.lblResultado.Font = new System.Drawing.Font("Showcard Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(475, 248);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(32, 36);
+            this.lblResultado.TabIndex = 11;
+            this.lblResultado.Text = "0";
+            // 
+            // frmCalculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(655, 458);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.cmdSalir);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.cmdMulti);
+            this.Controls.Add(this.cmdResta);
+            this.Controls.Add(this.cmdSuma);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblopera);
+            this.Controls.Add(this.lblv2);
+            this.Controls.Add(this.lblv1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "frmCalculadora";
+            this.Text = "Calculadora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblv1;
+        private System.Windows.Forms.Label lblv2;
+        private System.Windows.Forms.Label lblopera;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button cmdSuma;
+        private System.Windows.Forms.Button cmdResta;
+        private System.Windows.Forms.Button cmdMulti;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button cmdSalir;
+        private System.Windows.Forms.Label lblResultado;
     }
 }
 
--- Calculadora/Calculadora/Form1.cs
+++ Calculadora/Calculadora/Form1.cs
@@ -10,11 +10,22 @@ using System.Windows.Forms;
 
 namespace Calculadora
 {
-    public partial class Form1 : Form
+    public partial class frmCalculadora : Form
     {
-        public Form1()
+        static Operaciones op = new Operaciones();
+        public frmCalculadora()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
